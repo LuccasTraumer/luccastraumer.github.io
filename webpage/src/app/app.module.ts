9a399f8ca1662b3b39de62650da0a165bb4ee015
Lucas Jesus <cc19372@g.unicamp.br>
2020-10-13 22:39:01 -0300

    created apresentation component and init header

--- webpage/src/app/app.module.ts
+++ webpage/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StaticHeaderComponent } from './components/static-header/static-header.component';
+import { ApresentacaoComponent } from './components/apresentacao/apresentacao.component';
+import { LinksComponent } from './components/static-header/links/links.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StaticHeaderComponent,
+    ApresentacaoComponent,
+    LinksComponent
   ],
   imports: [
     BrowserModule,
---
+++ webpage/src/app/components/static-header/static-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-static-header',
+  templateUrl: './static-header.component.html',
+  styleUrls: ['./static-header.component.css']
+})
+export class StaticHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

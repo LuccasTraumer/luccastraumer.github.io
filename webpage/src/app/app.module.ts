8908e2b38550556b7cc2b7d8cdf1dcc796241560
Lucas Jesus <cc19372@g.unicamp.br>
2020-10-14 22:47:18 -0300

    init head and need cut img on assets to put in head

--- webpage/src/app/app.module.ts
+++ webpage/src/app/app.module.ts
@@ -6,13 +6,25 @@ import { AppComponent } from './app.component';
 import { StaticHeaderComponent } from './components/static-header/static-header.component';
 import { ApresentacaoComponent } from './components/apresentacao/apresentacao.component';
 import { LinksComponent } from './components/static-header/links/links.component';
+import { HeadComponent } from './components/head/head.component';
+import { AboutComponent } from './components/about/about.component';
+import { ResumeComponent } from './components/resume/resume.component';
+import { PortifolioComponent } from './components/portifolio/portifolio.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StaticHeaderComponent,
     ApresentacaoComponent,
-    LinksComponent
+    LinksComponent,
+    HeadComponent,
+    AboutComponent,
+    ResumeComponent,
+    PortifolioComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,

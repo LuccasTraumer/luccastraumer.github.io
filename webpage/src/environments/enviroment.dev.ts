71e8dc2aba2032260c5f40641e12abaa6aab7a89
Lucas <cc19372@g.unicamp.br>
2020-11-06 21:57:31 -0300

    add constant in env file and put on Constantes

--- webpage/src/environments/enviroment.dev.ts
+++ webpage/src/environments/enviroment.dev.ts
@@ -1,10 +1,25 @@
 export const environment = {
     production: false,
+    /** ICONS SOCIAL MEDIAS */
+
     iconSocialMediaFacebook: 'https://raw.githubusercontent.com/LuccasTraumer/luccastraumer.github.io-page/dev/webpage/src/assets/social-media/facebook.png',
     iconSocialMediaTwitter: 'https://raw.githubusercontent.com/LuccasTraumer/luccastraumer.github.io-page/dev/webpage/src/assets/social-media/twitter.png',
     iconSocialMediaInstagram: 'https://raw.githubusercontent.com/LuccasTraumer/luccastraumer.github.io-page/dev/webpage/src/assets/social-media/instagram.png',
     iconSocialMediaGithub: 'https://raw.githubusercontent.com/LuccasTraumer/luccastraumer.github.io-page/dev/webpage/src/assets/social-media/github.png',
     iconSocialMediaLinkedin: 'https://raw.githubusercontent.com/LuccasTraumer/luccastraumer.github.io-page/dev/webpage/src/assets/social-media/linkedin.png',
+
+    /** HOBBIES SOCIAL MEDIAS */
+
+    iconAppleHobbieList: 'https://raw.githubusercontent.com/LuccasTraumer/luccastraumer.github.io-page/da8576a48b978351723bed9527d776cf3ef80c43/webpage/src/assets/hobbies-icons/apple.svg',
+    iconCinematicHobbieList: 'https://raw.githubusercontent.com/LuccasTraumer/luccastraumer.github.io-page/da8576a48b978351723bed9527d776cf3ef80c43/webpage/src/assets/hobbies-icons/cinema.svg',
+    iconCoffeHobbieList: 'https://raw.githubusercontent.com/LuccasTraumer/luccastraumer.github.io-page/da8576a48b978351723bed9527d776cf3ef80c43/webpage/src/assets/hobbies-icons/coffe.svg',
+    iconControllerGameHobbieList: 'https://raw.githubusercontent.com/LuccasTraumer/luccastraumer.github.io-page/da8576a48b978351723bed9527d776cf3ef80c43/webpage/src/assets/hobbies-icons/game-controller.svg',
+    iconHeadphoneHobbieList: 'https://raw.githubusercontent.com/LuccasTraumer/luccastraumer.github.io-page/da8576a48b978351723bed9527d776cf3ef80c43/webpage/src/assets/hobbies-icons/headphone.svg',
+    iconMoneyHobbieList: 'https://raw.githubusercontent.com/LuccasTraumer/luccastraumer.github.io-page/da8576a48b978351723bed9527d776cf3ef80c43/webpage/src/assets/hobbies-icons/money.svg',
+    iconTravelHobbieList: 'https://raw.githubusercontent.com/LuccasTraumer/luccastraumer.github.io-page/da8576a48b978351723bed9527d776cf3ef80c43/webpage/src/assets/hobbies-icons/travel.svg',
+
+
+    /** LINKS SOCIAL MEDIAS */
 
     linkSocialMediaFacebook: 'https://www.facebook.com/LuccasTraumer',
     linkSocialMediaTwitter: 'https://twitter.com/LuccasTraumer',
@@ -12,8 +27,13 @@ export const environment = {
     linkSocialMediaGithub: 'https://github.com/LuccasTraumer',
     linkSocialMediaLinkedin: 'https://www.linkedin.com/in/luccastraumer/',
 
+
+    /** LINKS IMAGES */
+
     linkMyPersonalImage: 'https://avatars3.githubusercontent.com/u/16438051?s=400&u=fa0224315b18ada8435edf9ee06f2022f726bb6f&v=4',
     backgroundImageHead: 'https://raw.githubusercontent.com/LuccasTraumer/luccastraumer.github.io-page/dev/webpage/src/assets/developer-working-on-his-project.jpg',
     iconInfo: 'https://raw.githubusercontent.com/LuccasTraumer/luccastraumer.github.io-page/2a1c89fad1e8a4710cb38c81fce09747de9bc986/webpage/src/assets/info.svg',
+
+
 
 };

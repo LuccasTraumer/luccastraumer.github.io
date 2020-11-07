70758618f87ef00f6080bfd34b6000c9d222dade
Lucas <cc19372@g.unicamp.br>
2020-11-06 22:09:40 -0300

    put method to return path of images on list hobbies but need fix her size and align

--- webpage/src/app/components/about/list-hobbies/list-hobbies.component.ts
+++ webpage/src/app/components/about/list-hobbies/list-hobbies.component.ts
@@ -1,3 +1,4 @@
+import { Constantes } from './../../../../Utils/Constantes';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -11,5 +12,35 @@ export class ListHobbiesComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+
+  public getIconApple() {
+    return Constantes.iconAppleHobbieList;
+  }
+
+  public getIconCinematic() {
+    return Constantes.iconCinematicHobbieList;
+  }
+
+  public getIconCoffe() {
+    return Constantes.iconCoffeHobbieList;
+  }
+
+  public getIconControleGame() {
+    return Constantes.iconControllerGameHobbieList;
+  }
+
+  public getIconHeadphone() {
+    return Constantes.iconHeadphoneHobbieList;
+  }
+
+  public  getIconMoney() {
+    return Constantes.iconMoneyHobbieList;
+  }
+
+  public getIconTravel() {
+    return Constantes.iconTravelHobbieList;
+  }
+
 
 }

70d44f4a5433a39d703b567a84275196cc96db23
Lucas Jesus <cc19372@g.unicamp.br>
2020-12-13 21:18:17 -0300

    add a title in resume and make section scroll functional

--- webpage/src/app/app-routing.module.ts
+++ webpage/src/app/app-routing.module.ts
@@ -22,7 +22,10 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    useHash: true,
+    anchorScrolling: 'enabled'
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- webpage/src/app/components/static-header/static-header.component.ts
+++ webpage/src/app/components/static-header/static-header.component.ts
@@ -1,3 +1,4 @@
+import { ViewportScroller } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StaticHeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private viewportScroller: ViewportScroller) { }
 
   ngOnInit(): void {
+  }
+
+  onClickScroll(elementId: string) {
+    this.viewportScroller.scrollToAnchor(elementId);
   }
 
 }

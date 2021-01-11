1af91e6803eec0f903c17488dd299116a4006322
Lucas Jesus <cc19372@g.unicamp.br>
2021-01-10 21:32:52 -0300

    change uri to background image with optimization

--- webpage/src/environments/enviroment.dev.ts
+++ webpage/src/environments/enviroment.dev.ts
@@ -1,13 +1,13 @@
 export const environment = {
     production: false,
-    
+
     /** ICONS SOCIAL MEDIAS */
     iconSocialMediaFacebook: 'https://raw.githubusercontent.com/LuccasTraumer/luccastraumer.github.io-page/dev/webpage/src/assets/social-media/facebook.png',
     iconSocialMediaTwitter: 'https://raw.githubusercontent.com/LuccasTraumer/luccastraumer.github.io-page/dev/webpage/src/assets/social-media/twitter.png',
     iconSocialMediaInstagram: 'https://raw.githubusercontent.com/LuccasTraumer/luccastraumer.github.io-page/dev/webpage/src/assets/social-media/instagram.png',
     iconSocialMediaGithub: 'https://raw.githubusercontent.com/LuccasTraumer/luccastraumer.github.io-page/dev/webpage/src/assets/social-media/github.png',
     iconSocialMediaLinkedin: 'https://raw.githubusercontent.com/LuccasTraumer/luccastraumer.github.io-page/dev/webpage/src/assets/social-media/linkedin.png',
-    
+
     /** LINKS SOCIAL MEDIAS */
     linkSocialMediaFacebook: 'https://www.facebook.com/LuccasTraumer',
     linkSocialMediaTwitter: 'https://twitter.com/LuccasTraumer',
@@ -18,7 +18,7 @@ export const environment = {
 
     /** LINKS IMAGES */
     linkMyPersonalImage: 'https://avatars3.githubusercontent.com/u/16438051?s=400&u=fa0224315b18ada8435edf9ee06f2022f726bb6f&v=4',
-    backgroundImageHead: 'https://raw.githubusercontent.com/LuccasTraumer/luccastraumer.github.io-page/dev/webpage/src/assets/developer-working-on-his-project.jpg',
+    backgroundImageHead: 'https://raw.githubusercontent.com/LuccasTraumer/luccastraumer.github.io/main/webpage/src/assets/developer-working-on-his-project.jpg',
     backgroundimageContact: 'https://raw.githubusercontent.com/LuccasTraumer/luccastraumer.github.io/dev/webpage/src/assets/contact_bg.png',
     iconInfo: 'https://raw.githubusercontent.com/LuccasTraumer/luccastraumer.github.io-page/2a1c89fad1e8a4710cb38c81fce09747de9bc986/webpage/src/assets/info.svg',
     iconPortfolio: 'https://raw.githubusercontent.com/LuccasTraumer/luccastraumer.github.io/b2e1bd71be97e847ef9fbb1ff3a927a678d1ef64/webpage/src/assets/icons/portfolio.svg',

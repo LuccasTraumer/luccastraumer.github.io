787c49391ae3369ebf8cd0e52cd54c14b5fbac53
Lucas Jesus <cc19372@g.unicamp.br>
2021-04-17 14:14:22 -0300

    footer doen

--- page/src/app/personal-footer/personal-footer.module.ts
+++ page/src/app/personal-footer/personal-footer.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PersonalFooterComponent } from './personal-footer/personal-footer.component';
+import { SocialMediaModule } from '../social-media/social-media.module';
 
 
 
 @NgModule({
   declarations: [PersonalFooterComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    SocialMediaModule
   ],
   exports: [PersonalFooterComponent]
 })
--- page/src/app/personal-footer/personal-footer/personal-footer.component.ts
+++ page/src/app/personal-footer/personal-footer/personal-footer.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PersonalFooterComponent implements OnInit {
 
+  public readonly year = new Date().getFullYear();
   constructor() { }
 
   ngOnInit(): void {

7cd2b6207aed695b2d4048db8b4c171d33d91d09
Lucas Jesus <cc19372@g.unicamp.br>
2021-04-19 21:00:20 -0300

    with dynamic sentences

--- page/src/app/home/home/home.component.ts
+++ page/src/app/home/home/home.component.ts
@@ -7,10 +7,19 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
-  readonly sentences = ['Angular Developer', 'Fullstack Developer', 'Javascript & Typescript'];
-  index: number = 0;
-
   public sentence: string;
-  constructor() { }
+  readonly sentences = ['Angular Developer', 'Fullstack Developer', 'Javascript & Typescript', 'in love for mobile dev', 'in love for frontend dev'];
 
+  private index: number = 1;
+  private timer = 5000;
+
+  now:number;
+
+  constructor() {
+    this.sentence = this.sentences[this.index];
+    setInterval(() => {
+      console.log(this.index)
+      this.sentence = this.index > this.sentences.length-1 ? this.sentences[this.index = 0] : this.sentences[this.index++];
+    }, 3000);
+  }
 }

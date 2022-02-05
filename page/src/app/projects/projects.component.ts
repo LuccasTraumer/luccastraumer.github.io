6a850c912d025fe5d1d93f2f2c1600b575a27973
Lucas Jesus <cc19372@g.unicamp.br>
2022-02-05 10:41:04 -0300

    Icone das seta

--- page/src/app/projects/projects.component.ts
+++ page/src/app/projects/projects.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Constantes} from "../../utils/Constantes";
 
 @Component({
   selector: 'app-projects',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent implements OnInit {
+  readonly CONSTANTES = Constantes;
 
   constructor() { }
 

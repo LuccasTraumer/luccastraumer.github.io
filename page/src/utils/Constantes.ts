9c1ed1841e1feed842c926c49f36b0e72c103acb
Lucas Jesus <cc19372@g.unicamp.br>
2022-02-25 10:20:38 -0300

    Removendo as chamadas para o github para obtençao das imagens

--- page/src/utils/Constantes.ts
+++ page/src/utils/Constantes.ts
@@ -3,31 +3,30 @@ export class Constantes {
   public static readonly apiGithubRepository = 'https://api.github.com/users/luccastraumer/repos?page=1&per_page=100';
   public static readonly apiGithubOwner = 'https://api.github.com/users/luccastraumer';
 
-  public static readonly PATH_ICON_BLACK_MENU_HAMBURGUER = 'https://raw.githubusercontent.com/LuccasTraumer/luccastraumer.github.io/remade/page/src/assets/menu-hamburguer-black-png.png';
-  public static readonly PATH_ICON_WHITE_MENU_HAMBURGUER = 'https://raw.githubusercontent.com/LuccasTraumer/luccastraumer.github.io/remade/page/src/assets/menu-hamburguer-white-png.png';
+  public static readonly PATH_ICON_BLACK_MENU_HAMBURGUER = '/assets/menu-hamburguer-black-png.png';
 
-  public static readonly PATH_ICONE_ENVIAR_MENSAGEM = 'https://github.com/LuccasTraumer/luccastraumer.github.io/blob/novaPagina/page/src/assets/novaPagina/send-message-icon.png?raw=true';
-  public static readonly PATH_ICONE_ARROW = 'https://github.com/LuccasTraumer/luccastraumer.github.io/blob/novaPagina/page/src/assets/novaPagina/arrow-1.png?raw=true';
-  public static readonly PATH_ICONE_LINKEDIN = 'https://raw.githubusercontent.com/LuccasTraumer/luccastraumer.github.io/17042c6f4018cfea0a11f7245696e0764b59fb54/page/src/assets/novaPagina/linkedin-icon.svg';
-  public static readonly PATH_ICONE_TWITTER = 'https://raw.githubusercontent.com/LuccasTraumer/luccastraumer.github.io/17042c6f4018cfea0a11f7245696e0764b59fb54/page/src/assets/novaPagina/twitter-icon.svg';
-  public static readonly PATH_ICONE_INSTAGRAM = 'https://raw.githubusercontent.com/LuccasTraumer/luccastraumer.github.io/17042c6f4018cfea0a11f7245696e0764b59fb54/page/src/assets/novaPagina/instagram-icon.svg';
-  public static readonly PATH_ICONE_GITHUB = 'https://raw.githubusercontent.com/LuccasTraumer/luccastraumer.github.io/17042c6f4018cfea0a11f7245696e0764b59fb54/page/src/assets/novaPagina/github-icon.svg';
-  public static readonly IMAGEM_ABOUT = 'https://github.com/LuccasTraumer/luccastraumer.github.io/blob/novaPagina/page/src/assets/novaPagina/image-page.png?raw=true';
-  public static readonly ICONE_ANGULAR = 'https://raw.githubusercontent.com/LuccasTraumer/luccastraumer.github.io/a3abee5369e659a8a7a21d8c6f73d4d2192b58be/page/src/assets/novaPagina/icons8-angularjs.svg';
-  public static readonly ICONE_GCP = 'https://raw.githubusercontent.com/LuccasTraumer/luccastraumer.github.io/b7e4025f8843cf60c4b27166884b71e7e875fcbb/page/src/assets/novaPagina/google-cloud-platform-icon.svg';
-  public static readonly ICONE_JAVA = 'https://raw.githubusercontent.com/LuccasTraumer/luccastraumer.github.io/b7e4025f8843cf60c4b27166884b71e7e875fcbb/page/src/assets/novaPagina/java-icon.svg';
-  public static readonly ICONE_JAVASCRIPT = 'https://raw.githubusercontent.com/LuccasTraumer/luccastraumer.github.io/b7e4025f8843cf60c4b27166884b71e7e875fcbb/page/src/assets/novaPagina/javascript-icon.svg';
-  public static readonly ICONE_NGINX = 'https://github.com/LuccasTraumer/luccastraumer.github.io/blob/novaPagina/page/src/assets/novaPagina/nginx-icon.png?raw=true';
-  public static readonly ICONE_PYTHON = 'https://raw.githubusercontent.com/LuccasTraumer/luccastraumer.github.io/b7e4025f8843cf60c4b27166884b71e7e875fcbb/page/src/assets/novaPagina/python-icon.svg';
-  public static readonly ICONE_TYPESCRIPT = 'https://raw.githubusercontent.com/LuccasTraumer/luccastraumer.github.io/b7e4025f8843cf60c4b27166884b71e7e875fcbb/page/src/assets/novaPagina/typescript-icon.svg';
-  public static readonly ICONE_DOCKER = 'https://raw.githubusercontent.com/LuccasTraumer/luccastraumer.github.io/b7e4025f8843cf60c4b27166884b71e7e875fcbb/page/src/assets/novaPagina/docker-icon.svg';
-  public static readonly ICONE_CSS = 'https://github.com/LuccasTraumer/luccastraumer.github.io/blob/novaPagina/page/src/assets/novaPagina/css-icon.png?raw=true';
-  public static readonly ICONE_SETA_GITHUB = 'https://github.com/LuccasTraumer/luccastraumer.github.io/blob/novaPagina/page/src/assets/novaPagina/arrow-send-github.png?raw=true';
-  public static readonly ICONE_SETA_IMAGENS = 'https://github.com/LuccasTraumer/luccastraumer.github.io/blob/novaPagina/page/src/assets/novaPagina/arrow-volta.png?raw=true';
-  public static readonly ICONE_ARROW_HIRE = 'https://github.com/LuccasTraumer/luccastraumer.github.io/blob/novaPagina/page/src/assets/novaPagina/arrow_hire.png?raw=true';
-  public static readonly IMAGE_CONTACT_HI = 'https://github.com/LuccasTraumer/luccastraumer.github.io/blob/novaPagina/page/src/assets/novaPagina/contact.hi.png?raw=true';
-  public static readonly ICON_EMAIL = 'https://raw.githubusercontent.com/LuccasTraumer/luccastraumer.github.io/45efca40017a6b0f3d6ea8cf7f9414b8a7ab0051/page/src/assets/novaPagina/email-icon.svg';
-  public static readonly ICON_LOCATION = 'https://raw.githubusercontent.com/LuccasTraumer/luccastraumer.github.io/45efca40017a6b0f3d6ea8cf7f9414b8a7ab0051/page/src/assets/novaPagina/location-icon.svg';
-  public static readonly SEARCH_ICON = 'https://raw.githubusercontent.com/LuccasTraumer/luccastraumer.github.io/45efca40017a6b0f3d6ea8cf7f9414b8a7ab0051/page/src/assets/novaPagina/search-icon.svg';
+  public static readonly PATH_ICONE_ENVIAR_MENSAGEM = '/assets/novaPagina/send-message-icon.png';
+  public static readonly PATH_ICONE_ARROW = '/assets/novaPagina/arrow-1.png';
+  public static readonly PATH_ICONE_LINKEDIN = '/assets/novaPagina/linkedin-icon.svg';
+  public static readonly PATH_ICONE_TWITTER = '/assets/novaPagina/twitter-icon.svg';
+  public static readonly PATH_ICONE_INSTAGRAM = '/assets/novaPagina/instagram-icon.svg';
+  public static readonly PATH_ICONE_GITHUB = '/assets/novaPagina/github-icon.svg';
+  public static readonly IMAGEM_ABOUT = '/assets/novaPagina/image-page.png';
+  public static readonly ICONE_ANGULAR = '/assets/novaPagina/icons8-angularjs.svg';
+  public static readonly ICONE_GCP = '/assets/novaPagina/google-cloud-platform-icon.svg';
+  public static readonly ICONE_JAVA = '/assets/novaPagina/java-icon.svg';
+  public static readonly ICONE_JAVASCRIPT = '/assets/novaPagina/javascript-icon.svg';
+  public static readonly ICONE_NGINX = '/assets/novaPagina/nginx-icon.png';
+  public static readonly ICONE_PYTHON = '/assets/novaPagina/python-icon.svg';
+  public static readonly ICONE_TYPESCRIPT = '/assets/novaPagina/typescript-icon.svg';
+  public static readonly ICONE_DOCKER = '/assets/novaPagina/docker-icon.svg';
+  public static readonly ICONE_CSS = '/assets/novaPagina/css-icon.png';
+  public static readonly ICONE_SETA_GITHUB = '/assets/novaPagina/arrow-send-github.png';
+  public static readonly ICONE_SETA_IMAGENS = '/assets/novaPagina/arrow-volta.png';
+  public static readonly ICONE_ARROW_HIRE = '/assets/novaPagina/arrow_hire.png';
+  public static readonly IMAGE_CONTACT_HI = '/assets/novaPagina/contact.hi.png';
+  public static readonly ICON_EMAIL = '/assets/novaPagina/email-icon.svg';
+  public static readonly ICON_LOCATION = '/assets/novaPagina/location-icon.svg';
+  public static readonly SEARCH_ICON = '/assets/novaPagina/search-icon.svg';
 
 }

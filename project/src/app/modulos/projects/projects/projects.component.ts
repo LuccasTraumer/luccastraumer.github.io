e2853a5c774794533133d6e18522b431c397ff60
Lucas Jesus <cc19372@g.unicamp.br>
2023-03-03 07:50:50 -0300

    Ajuste no css

--- project/src/app/modulos/projects/projects/projects.component.ts
+++ project/src/app/modulos/projects/projects/projects.component.ts
@@ -10,6 +10,29 @@ export class ProjectsComponent implements OnInit {
 
   public readonly CONSTANTES = Constantes;
 
+  public readonly CARDS = [
+    {
+    name: 'E-commerc Landing Page',
+    alt: 'e-commerc project landring page',
+    path: this.CONSTANTES.ECOMMERC_PAGE
+    },
+    {
+      name: 'Landing Page',
+      alt: 'landring page project',
+      path: this.CONSTANTES.LANDING_PAGE_IMAGE
+    },
+    {
+      name: 'Card Crypto',
+      alt: 'Card Crypto Project',
+      path: this.CONSTANTES.CARD_IMAGE
+    },
+    {
+      name: 'Personal Page',
+      alt: 'personal page project',
+      path: this.CONSTANTES.PERSONAL_PAGE
+    },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
